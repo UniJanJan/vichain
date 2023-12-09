07262d5d969342aafcaf9497e01bb973f6595131
Jan Jankowicz <307596@uwr.edu.pl>
2023-12-09 14:53:10 +0100

    ui improvements

--- src/model/blockchain/blockchain.js
+++ src/model/blockchain/blockchain.js
@@ -61,18 +61,22 @@ class BlockWrapper {
         this.previousBlock = previousBlock;
         this.burnMap = previousBlock ? new DiscreteIntervalMap(previousBlock.burnMap) : new DiscreteIntervalMap();
         this.accountMap = previousBlock ? new Map(previousBlock.accountMap) : new Map();
+        this.spendableTokensSupply = previousBlock ? previousBlock.spendableTokensSupply : 0;
 
         this.block.blockBody.transactions.forEach(transaction => {
             var { sourceAddress, targetAddress, amount } = transaction.transactionBody;
 
             if (targetAddress.equals(burnAddress)) {
                 this.burnMap.push(amount, sourceAddress.toString(16));
+                this.spendableTokensSupply -= amount;
             }
 
 
             if (sourceAddress) {
                 var inputBalance = this.accountMap.get(sourceAddress.toString(16)) || 0;
                 this.accountMap.set(sourceAddress.toString(16), inputBalance - amount);
+            } else {
+                this.spendableTokensSupply += amount;
             }
 
             var outputBalance = this.accountMap.get(targetAddress.toString(16)) || 0;
