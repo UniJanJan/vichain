6ead109c3d2675cca50982c4cc9ba87b72008c3b
Jan Jankowicz <307596@uwr.edu.pl>
2023-12-20 18:08:06 +0100

    performance improvement

--- src/model/event/event_pool.js
+++ src/model/event/event_pool.js
@@ -1,7 +1,11 @@
+import { Queue } from "../../common/queue.js";
+
 export class EventPool {
+
     constructor() {
-        this.processableEvents = [];
+        this.processableEvents = new Queue();
         this.processingEvents = [];
         this.processedEvents = [];
     }
+    
 }
