0b5e245a4076bdd40abcfb2d21b15cb2c491e18a
Jan Jankowicz <307596@uwr.edu.pl>
2023-12-21 20:45:13 +0100

    refactor

--- src/metrics/leading_blocks_metrics.js
+++ src/metrics/leading_blocks_metrics.js
@@ -5,7 +5,7 @@ export class LeadingBlocksMetrics {
     constructor(network) {
         this.network = network;
         this.leadingBlocksMetrics = new Map();
-        this.metricsRetentionTime = 100000;
+        this.metricsRetentionTime = 50000;
     }
 
     collectMetrics(elapsedTime) {
@@ -13,7 +13,7 @@ export class LeadingBlocksMetrics {
             var leadingBlocksHashesColors = node.blockchain.leadingBlocks.map(leadingBlock => '#' + leadingBlock.block.blockHash.slice(0, 6));
             var nodeLeadingBlocksMetrics = this.leadingBlocksMetrics.get(node.id);
             if (!nodeLeadingBlocksMetrics) {
-                nodeLeadingBlocksMetrics = new IntervalMap();
+                nodeLeadingBlocksMetrics = new IntervalMap(false, this.metricsRetentionTime);
                 nodeLeadingBlocksMetrics.push(this.network.timer.currentTimestamp, []);
                 this.leadingBlocksMetrics.set(node.id, nodeLeadingBlocksMetrics);
             }
@@ -26,27 +26,21 @@ export class LeadingBlocksMetrics {
         var widthToTimeFactor = width / this.metricsRetentionTime;
 
         this.leadingBlocksMetrics.forEach((metrics, nodeId) => {
-            var intervals = metrics.getIntervals();
-
-            var currentIntervalIndex = intervals.length - 1;
-            var currentMetricsTotalTime = 0;
-
-            while (currentIntervalIndex >= 0 && currentMetricsTotalTime < this.metricsRetentionTime) {
-                var currentInterval = intervals[currentIntervalIndex];
-
-                var intervalTime = currentInterval.size;
-                if (currentMetricsTotalTime + intervalTime > this.metricsRetentionTime) {
-                    intervalTime = this.metricsRetentionTime - currentMetricsTotalTime;
+            metrics.forEachReversed((interval, currentMetricsTotalTime) => {
+                var shouldStop = false;
+                var intervalSize = interval.size;
+                if (currentMetricsTotalTime + intervalSize > this.metricsRetentionTime) {
+                    shouldStop = true;
+                    intervalSize = this.metricsRetentionTime - currentMetricsTotalTime;
                 }
 
                 graphics.beginPath();
-                graphics.rect(startX + currentMetricsTotalTime * widthToTimeFactor, startY + (nodeId - 1) * nodeMetricsHeight, intervalTime * widthToTimeFactor, nodeMetricsHeight);
-                graphics.fillStyle = currentInterval.object.length > 0 ? currentInterval.object[0] : '#ffffff';
+                graphics.rect(startX + (this.metricsRetentionTime - currentMetricsTotalTime - intervalSize) * widthToTimeFactor, startY + (nodeId - 1) * nodeMetricsHeight, intervalSize * widthToTimeFactor, nodeMetricsHeight);
+                graphics.fillStyle = interval.object.length > 0 ? interval.object[0] : '#ffffff';
                 graphics.fill();
 
-                currentIntervalIndex -= 1;
-                currentMetricsTotalTime += intervalTime;
-            }
+                return shouldStop;
+            });
 
         });
 
