790fe6e62a38a901900decb118ce848d432dc81c
Jan Jankowicz <307596@uwr.edu.pl>
2024-01-19 22:39:05 +0100

    committed transactions in metrics

--- src/metrics/transactions_state_metrics.js
+++ src/metrics/transactions_state_metrics.js
@@ -1,5 +1,6 @@
 import { LinkedList } from "../common/linked_list.js";
 import { EventHandler } from "../logic/handler/event_handler.js";
+import { BlockCreatingEvent } from "../model/event/block_creating_event.js";
 import { TransactionCreatingEvent } from "../model/event/transaction_creating_event.js";
 import { Metrics } from "./metrics.js";
 
@@ -13,9 +14,13 @@ export class TransactionsStateMetrics extends Metrics {
             timestamp: 0,
             metricsValue: 0
         })
+
+        this.committedTransactionsMetrics = new Map();
+
         this.maxValue = 5;
 
         eventHandlerDispositor.registerEventHandler(Node.name, TransactionCreatingEvent.name, new TransactionCreatingMetricsEventHandler(this.network, this.createdTransactionsMetrics));
+        eventHandlerDispositor.registerEventHandler(Node.name, BlockCreatingEvent.name, new BlockCreatingMetricsEventHandler(this.network, this.committedTransactionsMetrics));
     }
 
     collectMetrics(elapsedTime) {
@@ -46,6 +51,11 @@ export class TransactionsStateMetrics extends Metrics {
             }
 
             graphics.beginPath();
+            graphics.rect(startX + (this.metricsRetentionTime - currentMetricsTotalTime - intervalSize) * widthToTimeFactor, startY + height - metricsValue * heightToMaxValue, intervalSize * widthToTimeFactor, metricsValue * heightToMaxValue);
+            graphics.fillStyle = 'grey';
+            graphics.fill();
+
+            graphics.beginPath();
             graphics.moveTo(startX + (this.metricsRetentionTime - currentMetricsTotalTime) * widthToTimeFactor, startY + height - metricsValue * heightToMaxValue);
             graphics.lineTo(startX + (this.metricsRetentionTime - currentMetricsTotalTime - intervalSize) * widthToTimeFactor, startY + height - metricsValue * heightToMaxValue);
             graphics.strokeStyle = 'black';
@@ -65,6 +75,54 @@ export class TransactionsStateMetrics extends Metrics {
 
             return shouldStop;
         });
+
+
+        this.committedTransactionsMetrics.forEach((metrics, blockHash) => {
+            var lastTimestamp = currentTimestamp;
+            var currentMetricsTotalTime = 0;
+            var previousMetricsValue = null;
+            metrics.forEachReversed(element => {
+                var shouldStop = false;
+                var intervalSize = lastTimestamp - element.timestamp;
+                if (currentMetricsTotalTime + intervalSize > this.metricsRetentionTime) {
+                    shouldStop = true;
+                    intervalSize = this.metricsRetentionTime - currentMetricsTotalTime;
+                }
+    
+                var metricsValue = element.metricsValue;
+    
+                if (Math.ceil(metricsValue * 1.1) > this.maxValue) {
+                    this.maxValue = Math.ceil(metricsValue * 1.1);
+                }
+    
+                graphics.beginPath();
+                graphics.rect(startX + (this.metricsRetentionTime - currentMetricsTotalTime - intervalSize) * widthToTimeFactor, startY + height - metricsValue * heightToMaxValue, intervalSize * widthToTimeFactor, metricsValue * heightToMaxValue);
+                graphics.fillStyle = 'red';
+                graphics.fill();
+    
+                graphics.beginPath();
+                graphics.moveTo(startX + (this.metricsRetentionTime - currentMetricsTotalTime) * widthToTimeFactor, startY + height - metricsValue * heightToMaxValue);
+                graphics.lineTo(startX + (this.metricsRetentionTime - currentMetricsTotalTime - intervalSize) * widthToTimeFactor, startY + height - metricsValue * heightToMaxValue);
+                graphics.strokeStyle = '#' + blockHash.slice(0, 6);
+                graphics.lineWidth = 3;
+                graphics.stroke();
+    
+                if (previousMetricsValue) {
+                    graphics.beginPath();
+                    graphics.moveTo(startX + (this.metricsRetentionTime - currentMetricsTotalTime) * widthToTimeFactor, startY + height - metricsValue * heightToMaxValue);
+                    graphics.lineTo(startX + (this.metricsRetentionTime - currentMetricsTotalTime) * widthToTimeFactor, startY + height - previousMetricsValue * heightToMaxValue);
+                    graphics.strokeStyle = '#' + blockHash.slice(0, 6);
+                    graphics.lineWidth = 3;
+                    graphics.stroke();
+                }
+    
+                lastTimestamp = element.timestamp;
+                currentMetricsTotalTime += intervalSize;
+                previousMetricsValue = metricsValue;
+    
+                return shouldStop;
+            });
+        })
 
 
         graphics.beginPath();
@@ -108,16 +166,34 @@ class TransactionCreatingMetricsEventHandler extends EventHandler {
 
 class BlockCreatingMetricsEventHandler extends EventHandler {
 
-    // constructor(network, createdTransactionsMetrics) {
-    //     super(network);
-    //     this.createdTransactionsMetrics = createdTransactionsMetrics;
-    // }
+    constructor(network, committedTransactionsMetrics) {
+        super(network);
+        this.committedTransactionsMetrics = committedTransactionsMetrics;
+    }
 
-    // handle(processingNode, processedEvent, baton) {
-    //     var createdTransactionsSoFar = this.createdTransactionsMetrics.getLastElement().metricsValue;
-    //     this.createdTransactionsMetrics.push({
-    //         timestamp: baton.createdTransaction.transactionBody.creationTimestamp,
-    //         metricsValue: createdTransactionsSoFar + 1
-    //     });
-    // }
+    handle(processingNode, processedEvent, baton) {
+        if (baton.createdBlock && baton.isBlockAppended) {
+            var committedTransactionsForBlockMetrics = this.committedTransactionsMetrics.get(baton.createdBlock.blockBody.previousBlockHash);
+
+            if (!committedTransactionsForBlockMetrics) {
+                committedTransactionsForBlockMetrics = new LinkedList();
+                committedTransactionsForBlockMetrics.push({
+                    timestamp: 0,
+                    metricsValue: 0
+                });
+                this.committedTransactionsMetrics.set(baton.createdBlock.blockHash, committedTransactionsForBlockMetrics);
+            }
+
+            var committedTransactionsSoFar = committedTransactionsForBlockMetrics.getLastElement()?.metricsValue || 0;
+
+            committedTransactionsForBlockMetrics.push({
+                timestamp: baton.createdBlock.blockBody.creationTimestamp,
+                metricsValue: committedTransactionsSoFar + baton.createdBlock.blockBody.transactions.length - 1
+            });
+
+            this.committedTransactionsMetrics.set(baton.createdBlock.blockHash, committedTransactionsForBlockMetrics);
+            this.committedTransactionsMetrics.delete(baton.createdBlock.blockBody.previousBlockHash);
+            
+        }
+    }
 }
